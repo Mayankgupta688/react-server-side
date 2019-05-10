15776d149c75dd5ca8b0880dc4d0f608ff51e008
Mayank Gupta (IBAM) <Mayank.Gupta@iontrading.com>
2019-05-10 17:46:35 +0530

    Adding Server Side Rendering

--- src/AppComponent.js
+++ src/AppComponent.js
@@ -4,21 +4,35 @@ import axios from "axios";
 
 export default class AppComponent extends React.Component {
 
-    constructor() {
-        super();
+    static async getInitialProps() {
+        const dataPromise = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve({
+                    data: "MAyank"
+                })
+            }, 6000)
+        })
+
+        return dataPromise;
+    }
+
+    constructor(props) {
+        super(props);
         
         this.state = {
-            employeeDetails: []
+            employeeDetails: [], 
         }
     }
 
-    componentDidMount() {
-        axios.get("http://5c055de56b84ee00137d25a0.mockapi.io/api/v1/employees").then((empData) => {
+    
 
-            this.setState({
-                employeeDetails: empData.data
-            })
-        })
+    componentDidMount(props) {
+        // axios.get("http://5c055de56b84ee00137d25a0.mockapi.io/api/v1/employees").then((empData) => {
+
+        //     this.setState({
+        //         employeeDetails: empData.data
+        //     })
+        // })
     }
 
     render() {
